1a468707184305b57557bfc6c417a7178eee899e
Ami rafi <kh9961734@gmail.com>
2024-02-14 08:15:52 +0600

    country information

--- script.js
+++ script.js
@@ -1,17 +1,76 @@
-let form = document.getElementById('form');
+fetch('https://restcountries.com/v3.1/all')
+ .then(res => res.json())
+ .then((res) => {
 
-form.addEventListener('submit', function(e){
-
-e.preventDefault();
-
-var title = e.target.title.value;
-var url = e.target.url.value;
-var pdes = e.target.pdes.value;
-var descrip = e.target.descrip.value;
+    print(res)})
 
 
-console.log(title, url, pdes, descrip);
+function print(countries){
+
+    let root = document.getElementById('root');
+
+    let htmlCode = ' ';
+    
+    countries.forEach(country => {
+        htmlCode+=  make(country)
+    });
+
+    root.innerHTML= htmlCode;
+
+
+    console.log(countries)
+}
+
+
+
+function make(country){
+
+    
+    const {name, flags, capital, population,region }=country;
+
+   
+
+    let html = `
+        <div class="result">
+            <img src='${flags.png}  '/>
+            <h3>Name: ${name.common} </h3>
+            <h4>Capital: ${capital} </h4>
+            <h5>Population: ${population}</h5>
+            <h6>Region: ${region}</h6>
+            
+    
+        </div>
+    
+    
+    `;
+
+
+    return html;
+}
+
+
+let form = document.getElementById('form')
+
+form.addEventListener('submit', (e)=>{
+
+    e.preventDefault();
+
+    const text = e.target.searchBox.value;
+
+    const searchvalue = text.toLowerCase();
+
+    
+
+    fetch(`https://restcountries.com/v3.1/name/${searchvalue}`)
+        .then(res=> res.json())
+        .then(res => print(res))
 
 
 
 })
+
+
+
+
+
+
